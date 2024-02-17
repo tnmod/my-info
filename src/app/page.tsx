1f58b653f56170f023eb8a31e1a70d6326946c31
Nguyễn Phú Tín <tnmod2003@gmail.com>
2024-02-17 16:50:12 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <p className={styles.text}>tnmod</p>
         </div>
         <div className={`absolute transform transition-all delay-150 duration-500 translate-y-0 justify-center items-center flex opacity-0 z-0 h-52 w-full ${isActive && "translate-y-96 opacity-100"}`}>
-          <div className={`w-8 h-full delay-500 transform transition-all duration-700 ${isActive && "w-11/12 rounded-2xl"}`}>
+          <div className={`w-8 h-full delay-500 transform transition-all duration-700 ${isActive && "w-full rounded-2xl"}`}>
             <div className={`h-full w-full bg-white delay-700 transform transition-all duration-1000  ${isActive && " h-96 rounded-2xl"}`}>
             </div>
           </div>
